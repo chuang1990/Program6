548c06c89da52805a71ea9c96b3b66f9f662f6e4
Cathilufia <c_huang4@post.massbay.edu>
2014-12-08 09:54:40 -0500

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import java.util.Stack;
+
 /**
  * Created by catherine huang on 12/4/14.
  */
@@ -10,40 +10,93 @@ public class Room {
 
     String roomNumber;
 
-    ArrayList<Integer> playerInRoom = new ArrayList<Integer>();
+    ArrayList<String> playerInRoom = new ArrayList<String>();
 
-    public HashMap<Integer, Object> playerList;
+    public HashMap<String, Object> playerList;
 
+
+    /**
+     * Constructor
+     * @param number String
+     */
     public Room(String number){
-        this.playerList = new HashMap<Integer, Object>();
+        this.playerList = new HashMap<String, Object>();
         this.roomNumber = number;
     }
 
+    /**
+     * returns the name of the Room
+     * @return String room name
+     */
     public String getRoomName(){
         return "Room "+roomNumber+" ";
     }
 
+    /**
+     * adds player to the room
+     * @param p Player
+     */
     public void addPlayer(Player p) {
-        playerList.put(p.hashCode(), p);
-        playerInRoom.add(p.hashCode());
-        System.out.print(p.toString()+" ");
-        System.out.print(getRoomName());
+        playerList.put(p.getName(), p);
+        playerInRoom.add(p.getName());
+//        System.out.print(p.toString()+" ");
+//        System.out.print(getRoomName());
     }
 
     public void removePlayer(Player p){
         playerList.remove(p.hashCode(),p);
     }
 
-    public void getPlayerInRoom(){
-        //StringBuilder sb = new StringBuilder("");
+    /**
+     * prints out the player in the room.
+     */
+    public String getPlayerInRoom(){
+        String playersString = "Players in Room " + roomNumber + ":\n";
         if(playerList.size()==0)
-            System.out.print("EMPTY!");
+            playersString += "EMPTY!\n";
         for(int i = 0; i< playerList.size(); i++){
            //String key = playerList.get(playerInRoom.peek()).toString();
            // System.out.print(key);
-           System.out.print(playerList.get(playerInRoom.get(i))+"");
+           playersString += playerList.get(playerInRoom.get(i))+"\n";
             //sb.append(playerList.get(key)+"");
         }
+        return playersString;
         //return sb.toString();
     }
-}
+
+
+//    public ArrayList<String> movePlayers() {
+//        if(playerInRoom.size() == 0) {
+//            return null;
+//        }
+//        else {
+//            ArrayList<String> tempList = new ArrayList<String>();
+//            for(String p: playerList) {
+//                Player tempPlayer = playerList.get(p);
+//                if() {
+//                    tempList.add(p.getName());
+//                    playerList.remove(p.getName());
+//                    playerInRoom.remove(p);
+//                }
+//            }
+//            return tempList;
+//        }
+//    }
+
+    /**
+     * empty the room
+     * @return String
+     */
+    public String clearRoom(){
+        if(playerList.size() == 0)
+            return "Nothing to delete\n";
+        for(String playerCode: playerInRoom){
+            playerList.remove(playerCode);
+        }
+        playerInRoom.clear();
+        return "Room Emptied\n";
+    }
+
+
+
+}//end of Room
